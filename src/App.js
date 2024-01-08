95dac2d85a2ed8eeeab6e78a5c92a84cff933b24
jye104 <84173008+jye104@users.noreply.github.com>
2024-01-08 13:53:51 +0900

    React에서 배열 사용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import "./App.css";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "데니",
+    content: "보통날",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "호영",
+    content: "보통날",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "쭈니",
+    content: "보통날",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "태우",
+    content: "보통날",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 5,
+    author: "계상",
+    content: "보통날",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
